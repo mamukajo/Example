4a8e6919d8252c0f9233dc25285774a412d00057
joshivinay619@gmail.com <joshivinay619@gmail.com>
2022-06-27 15:43:54 +0530

    final commit

---
+++ objectDestructuring.js
@@ -0,0 +1,26 @@
+let emp={
+
+
+ename:"Jhon",
+eid:123,
+edept:"Elcetrical",
+
+}
+
+
+let eDept={
+
+d1:"mech",
+d2:"civil",
+d3:"cse",
+d4:"eee"
+
+} 
+
+let {ename="Jhon",eid=154,edept="cse"}=emp;
+
+console.log(ename)
+console.log(eid)
+console.log(edept)
+
+
