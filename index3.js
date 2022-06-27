b131f8664a7efb9d4930091b4d17783dadbcccba
joshivinay619@gmail.com <joshivinay619@gmail.com>
2022-06-27 12:12:18 +0530

    SecondJS

---
+++ index3.js
@@ -0,0 +1,26 @@
+let developer1={
+
+fname:"jhon",
+sname:'cena',
+age:45,
+location:"USA",
+
+role:{primaryskill:"front end",secondaryskill:"backend"}
+
+}
+
+//Deep COPY
+/*let developer2=Object.assign({},developer1)
+
+developer2.fname="Joshi"
+developer2.sname="vinay" */
+
+//Nested OBJECT
+
+let developer2=JSON.parse(JSON.stringify(developer1))
+
+developer2.role.primaryskill="java"
+
+developer1.role.secondaryskill="DQL"
+console.log(developer2)
+console.log(developer1)
