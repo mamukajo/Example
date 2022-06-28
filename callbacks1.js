71be4c032ffbd7439fbe291b420edaafef7ad715
joshivinay619@gmail.com <joshivinay619@gmail.com>
2022-06-28 11:38:44 +0530

    final commit

--- callbacks1.js
+++ callbacks1.js
@@ -1,4 +1,6 @@
-import fetch from 'node-fetch';
+//import fetch from 'node-fetch'
+//import * as fs from 'fs'
+//const fs=require('fs')
 
 setTimeout(()=>{console.log("waited for 1 Second")},1000)
 
@@ -11,7 +13,11 @@ setTimeout(()=>{console.log("3");
                     1000)
                    
 
- fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+ /*fetch('https://pokeapi.co/api/v2/pokemon/ditto').then((res)=>{res.json()})
  .then((data)=>{console.log('data')})
  
- .catch((err)=>{console.log("failed")})
+ .catch((err)=>{console.log('failed')}) */
+
+
+
+
