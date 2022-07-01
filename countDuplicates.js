cc120cd2b518649d7145539f259ffb02c6c95868
joshivinay619@gmail.com <joshivinay619@gmail.com>
2022-07-01 12:26:37 +0530

    countDuplicates commit

---
+++ countDuplicates.js
@@ -0,0 +1,28 @@
+const arr=['one','two','three','one','one','two']
+
+const count={}
+
+arr.forEach(value=>{
+
+        count[value]=(count[value]|| 0) + 1
+
+
+
+})
+
+console.log(count)
+
+
+
+
+
+const cars=['BMW','volksvagon','swift','maruthi','BMW','volksvagon','swift','swift']
+
+const counter={}
+
+cars.forEach(value=>{
+
+counter[value]=(counter[value]|| 0) +1
+
+})
+console.log(counter)
