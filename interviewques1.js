d2225d6143e2e750c2c706892638e58f0972ad98
joshivinay619@gmail.com <joshivinay619@gmail.com>
2022-07-01 12:26:54 +0530

    commit

---
+++ interviewques1.js
@@ -0,0 +1,50 @@
+//Var Senario
+/*for(var i=0;i<5;i++)
+{
+setTimeout(()=>{console.log(i)
+},i*1000)
+
+}*/
+
+//Let Senario
+
+/*for(let i=0;i<5;i++)
+{
+setTimeout(()=>{console.log(i)
+},i*1000)
+
+} */
+
+
+//Object destructuring swap two variables
+   
+/*let a=10;
+   let b=20;
+   [a,b]=[b,a]
+   console.log(a)
+   console.log(b) */
+
+// Remove duplicate elements in array
+
+/*
+let x=[1,2,3,2,3,4,3,4,5]
+//console.log(new Set(x))
+let rmduplicates=x.filter((value,index)=>{
+return x.indexOf(value)==index
+})
+console.log(rmduplicates)
+*/
+
+// Reverse a array in js
+ /* 
+ let x=[2,3,4,3,5]
+var y=[]
+//console.log(x.reverse())
+
+for(var i=x.length-1;i>=0;i--)
+{
+y.push(x[i])
+}
+console.log(y); */
+
+//count of duplicate elements
